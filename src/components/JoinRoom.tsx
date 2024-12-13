db806d2d2f11ba2680d2e564fd3d1e8928750877
Dalip Tandon <daliptandon8@gmail.com>
2024-12-14 00:09:31 +0530

    join room

---
+++ src/components/JoinRoom.tsx
@@ -0,0 +1,26 @@
+import axios from "axios";
+import { useEffect, useState } from "react"
+import { BASE_URL } from "../utils/config";
+
+
+
+const JoinRoom=()=>{
+    const [roomId,setRoomId]=useState("");
+    const JoinRoom=async()=>{
+        const res=await axios.post(BASE_URL+"/joinroom",{roomId},{withCredentials:true});
+        console.log(res.data);
+        
+    }
+    useEffect(()=>{
+        JoinRoom
+    })
+    return(
+        <div className=" h-96 w-full flex justify-center items-center">
+
+        <input  value={roomId} onChange={(e)=>setRoomId(e.target.value)} type="text" className="h-10 w-96 border border-black bg-gray-100 p-4" placeholder="Enter Room Code"/>
+        <button onClick={()=>JoinRoom()} className="mx-4 bg-blue-500 p-2 rounded-lg"  >Join Room</button>
+        </div>
+    )
+}
+
+export default JoinRoom;
